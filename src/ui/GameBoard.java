5a20565692d95d2b09eb6b71f579697617b6418a
Dylan Wothal <dylan-pc@South_Lehman_CC.(none)>
2014-02-06 01:36:42 -0500

    MAde major changes to GameBoard now that pushing and pulling is working properly: 1) Window opens on run and closes when program ends (and vise versa) 2) Button clickable added 3) paint method working properly

--- src/ui/GameBoard.java
+++ src/ui/GameBoard.java
@@ -1,9 +1,8 @@
 package ui;
 
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class GameBoard extends JPanel {
@@ -11,18 +10,46 @@ public class GameBoard extends JPanel {
 	public JFrame frame;
 	
 	public GameBoard(){
-		frame = new JFrame("Quoridor");
-		this.setPreferredSize(new Dimension(500,500));
-		frame.add( this );
-		frame.setVisible(true);
-		frame.setResizable(false);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setPreferredSize( new Dimension( 500,500 ) );	//Makes the JPanel 500px * 500px
+		this.setLayout( new FlowLayout() );
+		setFrameStats();
 	}
 	
-	public static void main(String[] args) {
+	public void setFrameStats(){
+		frame = new JFrame( "Quoridor" ); 		//Title of JFrame window is "Quoridor"
+		frame.add( this );						//Adds this JPanel to JFrame
+		frame.setVisible( true );				//Sets Frame to visible
+		frame.setResizable( false );			//Doesn't allow resizing of frame
+		frame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );	//when frame is closed, the program terminates
+		try { Thread.sleep(100); } catch (InterruptedException e) {}
+		frame.setLocation( 150, 150 );
+		addOtherJObjects();		
+		try { Thread.sleep(100); } catch (InterruptedException e) {}
+		frame.pack();	//collapses frame to minimum size around all JObjects inside it
+	}
+	
+	public void addOtherJObjects(){
+		JButton clickMe = new JButton();
+		clickMe.addActionListener( new ActionListener(){
+			public void actionPerformed( ActionEvent e ) {
+				JOptionPane.showMessageDialog(null,  "HELLO", "THIS MESSAGE", JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		clickMe.setPreferredSize( new Dimension( 250, 250 ) );
+		this.add( clickMe, FlowLayout.LEFT );
+	}
+	
+	
+	
+	public static void main( String[] args ) {
+		// Auto-generated method stub
 		new GameBoard();
 
 	}
-
+	
+	//OverRidden for JPanel graphics
+	public void paint( Graphics g ){
+		g.setColor( Color.black );
+		g.fillRect( 0, 0, 500, 500 );
+	}
 }
