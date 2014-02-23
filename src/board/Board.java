2da84635f8122712bd36e2b98e33612057871a86
Eli Donahue <eli.j.donahue@gmail.com>
2014-02-23 16:50:53 -0500

    Created game and board classes. Commented in instance variables and proposed functionality.

---
+++ src/board/Board.java
@@ -0,0 +1,25 @@
+/* Author: Eli Donahue
+ * This class exists as a data container to track data about the
+ * contents of the board and to provide methods for players to 
+ * interact with the board.
+ */
+
+package board;
+
+public class Board {
+	
+	// Instance variables
+	// matrix of horizontal walls
+	// matrix of vertical walls
+	// matrix of player pieces
+	
+	// there must be a better way of tracking data
+	// simple bitmap maybe?
+	
+	// Methods
+	// constructor
+	// is empty
+	// place piece
+	// place wall
+	// validate moves
+}
---
+++ src/game/Game.java
@@ -0,0 +1,29 @@
+/* Author: Eli Donahue
+ * This class exists as the "main" program. A new game begins when
+ * a Game object is initialized. The Game will handle serve as the
+ * client.
+ */
+
+package game;
+
+public class Game {
+	
+	// Instance variables
+	// Board to track data about board's contents
+	// Player[] to hold players
+	// int players
+	// int numWalls
+	
+	// Methods
+	
+	// constructor
+	// exit game (report errors if any)
+	// client/server communication methods
+	// report errors
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
