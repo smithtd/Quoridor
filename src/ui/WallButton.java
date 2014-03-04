30555cb7f35d9a9a868063dce88878e04b6574a1
Dylan Woythal <woythadc194@potsdam.edu>
2014-03-04 11:59:38 -0500

    Still workign on the vert/horz wall selection and having to select an option: if I havent fixed it by the next time someone else pulls go to the //FIXME in the code (marked in blue on the sidebar) and see if you can take a crack at it

--- src/ui/WallButton.java
+++ src/ui/WallButton.java
@@ -35,15 +35,23 @@ public class WallButton extends JButton {
 		addActionListener( new ActionListener(){
 			public void actionPerformed( ActionEvent e ) {
 				//creates walls vertically
+				
 				// swapped out message reporting pos for dialog with options
 				JFrame tempFrame = new JFrame( " Wall Options" );
 				JPanel p1 = new JPanel(new GridLayout(3,5));
 				JPanel p2 = new JPanel(); 
+				
 				// create an array of radio button options
 				JRadioButton rb1 = new JRadioButton("Place a horizontal wall starting here.");
 				JRadioButton rb2 = new JRadioButton("Place a vertical wall starting here.");
 				JRadioButton rb3 = new JRadioButton("Neither.");
-				// add the buttons to the panel
+				
+				// setSelected = false
+				rb1.setSelected(false);
+				rb2.setSelected(false);
+				rb3.setSelected(false);
+				
+				// add the buttons to the panel and buttongroup
 				ButtonGroup btnGrp = new ButtonGroup();
 				p1.add(rb1);
 			    p1.add(rb2);
@@ -51,44 +59,75 @@ public class WallButton extends JButton {
 			    btnGrp.add(rb1);
 			    btnGrp.add(rb2);
 			    btnGrp.add(rb3);
+			   
 			    // call a JOptionPane to display the radio buttons
 			    JOptionPane.showMessageDialog(null,p1);
 			    
 			    JRadioButton ready = new JRadioButton("Okay!", false);
 				p2.add( ready ); 
 				
-				while(true){
-					if( ready.isSelected() && !rb1.isSelected() && !rb2.isSelected() && !rb3.isSelected() )
+				/*
+				 * FIXME
+				 * 
+				 * While none of the button group radio buttons are selected you 
+				 * cannot select the okay button
+				 */
+				while( ready.isSelected() == false ){
+					if( ready.isSelected() && !(rb1.isSelected() || rb2.isSelected() || rb3.isSelected() ) ){
 				    	ready.setSelected( false );
-					else if( ready.isSelected() && (rb1.isSelected() || rb2.isSelected()) || !rb3.isSelected() ){
-						System.out.println( "X:" + x + " Y:" + y );
+					}else if( ready.isSelected() && (rb1.isSelected() || rb2.isSelected()) || !rb3.isSelected() ){
+						/*
+						 * 
+						 * THE SWITCHING OF THE X AND Y COORDINATES IS NOT 
+						 * ACCIDENTAL DUE TO HOW MATRICIES WORK IN THIS CASE!!
+						 * THE FIRST VALUE IS ACTUALLY THE VERTICAL AND SECOND 
+						 * IS THE HORIZONTAL
+						 * 
+						 * It doesn't make sense but thats how this
+						 *
+						 *		cont.getHorzWalls()[x][y+1];
+						 *		cont.getHorzWalls()[x][y];
+						 *
+						 * changes the horizontal walls even though it looks 
+						 * like the vertical should be changed
+						 * 
+						 */
 						if( rb1.isSelected() ){
-//							try{
+							try{
 								cont.getHorzWalls()[x][y+1].setBackground( Color.GREEN );
 								cont.getHorzWalls()[x][y+1].repaint();
 								cont.getHorzWalls()[x][y].setBackground(  Color.GREEN );
 								cont.getHorzWalls()[x][y].repaint();
-//							}catch(Exception ex){
-//								System.out.println("ERROR IN HORZ");
-//							}
+							}catch(Exception ex){
+								System.out.println("ERROR IN HORZ");
+							}
 						}else if( rb2.isSelected() ){
-//							try{
+							try{
 								cont.getVertWalls()[x][y].setBackground( Color.GREEN );
 								cont.getVertWalls()[x][y].repaint();
 								cont.getVertWalls()[x+1][y].setBackground( Color.GREEN );
 								cont.getVertWalls()[x+1][y].repaint();
-//							}catch(Exception ex){
-//								System.out.println("ERROR IN VERT");
-//							}
+							}catch(Exception ex){
+								System.out.println("ERROR IN VERT");
+							}
 						}else if( rb3.isSelected() ){
 							System.out.println("NONE SELECTED");
 						}
+						
+						//FIXME Stuck in never ending loop
+						System.out.println("DEBUG 1");
 						tempFrame.dispose();
-						return;
-					}else
+						
+					}else{
 						try{
+							/*
+							 * Just to stop the system from lagging. The pauses 
+							 * stop it from working hard to do nothing while 
+							 * waiting for a selection to be made
+							 */
 							Thread.sleep(100);
 						}catch(Exception e2){}
+					}
 				}
 			}
 		});
