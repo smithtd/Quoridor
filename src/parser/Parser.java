cc42359f22c77998db561a259b61cf43f748252b
Marc Dean <deanmarc25@yahoo.com>
2014-03-16 21:48:18 -0400

    Starting work on the parser/parsertest file
    Parser will handle input from the move-servers in order to feed it
    to and understand it by the client.

---
+++ src/parser/Parser.java
@@ -0,0 +1,18 @@
+/**
+ * Quoridor String Parser
+ * @author marc dean jr
+ * Team - 511 tactical
+ */
+
+package parser;
+
+
+/**
+ *  The parser will verify moves are input correctly and send them off
+ *  to be verified that they are legal. It will do the same for wall
+ *  placements.
+ *
+ */
+public class Parser {
+
+}
