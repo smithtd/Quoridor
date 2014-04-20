df7526e75cf20ac04cd735c38c20a5c494208bb8
Marc <deanmarc25@yahoo.com>
2014-04-20 19:14:49 -0400

    Starting the game client.. added an args check and usage printout.

--- src/network/GameClient.java
+++ src/network/GameClient.java
@@ -23,5 +23,23 @@ public class GameClient {
 	
 	
 	
+	public static void main(String[] args) {
+		
+		if(args.length != 2 || args.length != 4)
+			usage();
+		
+		
+	}
+
+	/** 
+	 * Prints out the required way to start the gameClient
+	 */
+	private static void usage() {
+		System.out.println("\n\tTo start a game: \n\t$ java GameClient <ipp1> " +
+				"<ipp2> [<ipp3> <ipp4>]\n\n\tIPP = machine:port of players. " +
+				"Must have 2 or 4.\n");
+	}
+	
+	
 
 }
