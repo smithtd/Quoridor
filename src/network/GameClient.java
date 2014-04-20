288e8042aa3c9376976d689229a3143a9749421b
Marc Dean <deanmarc25@yahoo.com>
2014-04-19 21:50:32 -0400

    Starting the networking portion of the project again..

---
+++ src/network/GameClient.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * GameClient - Quoridor
+ * Team - 511Tactical
+ * @author marc
+ * 
+ */
+
+package network;
+
+/**
+ *
+ * The GameClient is the "game player" or a display GUI. The model of
+ * starting is that the client starts and receives two machine:port pairs
+ * where two (or four) move servers live. It then contacts the servers
+ * (giving each its player number and the number of competitors at this
+ * time). 
+ *
+ */
+public class GameClient {
+
+}
--- src/network/QuoridorClient.java
+++ src/network/QuoridorClient.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
