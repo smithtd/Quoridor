a61fc0b7b61ffc0d853dc1d9ff18e162299b11b3
Marc Dean <deanmarc25@yahoo.com>
2014-04-21 09:35:46 -0400

    When booting a player it throws a fit and prints out a bunch of garbage.
    I changed that. :)

--- src/ui/StatButton.java
+++ src/ui/StatButton.java
@@ -12,10 +12,10 @@ import main.Game;
 
 @SuppressWarnings("serial")
 public class StatButton extends JButton {
-	
+
 	public int playerNum;
 	public Color c;
-	
+
 	public StatButton(int pNum){
 		super();
 		this.setOpaque(true);
@@ -23,7 +23,7 @@ public class StatButton extends JButton {
 		this.playerNum = pNum;
 		c = Game.getPlayerAry().get(playerNum).getColor();
 		this.setBackground( c );
-		
+
 		this.setForeground( ( c==Color.red || c== Color.blue ?  Color.WHITE : Color.BLACK ) );
 		this.setText("Walls remaining: " + Game.getCurrPlayer().getWalls());
 
@@ -32,36 +32,39 @@ public class StatButton extends JButton {
 		else
 			this.setPreferredSize( new Dimension( GameBoard.getRightBarDim().width, GameBoard.getRightBarDim().height/4 ) );
 	}
-	
+
 	public void updateWalls(){
 		this.setText("Walls remaining: " + Game.getCurrPlayer().getWalls());
 	}
-	
+
 	public void paint( Graphics g ){
-		//Color block of player
-		g.setColor( c );
-		g.fillRect( 0, 0, this.getWidth(), this.getHeight() );
-		
-		//Black bar on side
-		g.setColor( Color.black );
-		g.fillRect( 0, 0, 1, this.getHeight() );
+		try {
+			//Color block of player
+			g.setColor( c );
+			g.fillRect( 0, 0, this.getWidth(), this.getHeight() );
 
-		//Box around text
-		int increment = 150;
-		int red = (c.getRed() + increment>256 ? c.getRed() : c.getRed() + increment );
-		int green = (c.getGreen() + increment>256 ? c.getGreen() : c.getGreen() + increment );
-		int blue = (c.getBlue() + increment>256 ? c.getBlue() : c.getBlue() + increment );
-		g.setColor( new Color( red, green, blue ) );
-		g.fillRect( Game.WallGap, Game.WallGap, this.getWidth()-Game.WallGap*2, this.getHeight()-Game.WallGap*2 );
-		
-		//Text
-		g.setFont( new Font( "" , Font.BOLD, (GameBoard.getRightBarDim().width==180 ? 10 : (int)(Game.PlayerWidth*(12.0/50.0)) ) ) );
-		FontMetrics fm = g.getFontMetrics( g.getFont() );
-		String s = "Walls remaining: " + Game.getPlayerAry().get(playerNum).getWalls();
-		g.setColor( Color.BLACK );
-		g.drawString( s, (this.getWidth()/2 - fm.stringWidth( s )/2), (this.getHeight()/2+fm.getHeight()/2) );
+			//Black bar on side
+			g.setColor( Color.black );
+			g.fillRect( 0, 0, 1, this.getHeight() );
 
-		
-//		g.drawString( s, 0, 0 );
+			//Box around text
+			int increment = 150;
+			int red = (c.getRed() + increment>256 ? c.getRed() : c.getRed() + increment );
+			int green = (c.getGreen() + increment>256 ? c.getGreen() : c.getGreen() + increment );
+			int blue = (c.getBlue() + increment>256 ? c.getBlue() : c.getBlue() + increment );
+			g.setColor( new Color( red, green, blue ) );
+			g.fillRect( Game.WallGap, Game.WallGap, this.getWidth()-Game.WallGap*2, this.getHeight()-Game.WallGap*2 );
+
+			//Text
+			g.setFont( new Font( "" , Font.BOLD, (GameBoard.getRightBarDim().width==180 ? 10 : (int)(Game.PlayerWidth*(12.0/50.0)) ) ) );
+			FontMetrics fm = g.getFontMetrics( g.getFont() );
+			String s = "Walls remaining: " + Game.getPlayerAry().get(playerNum).getWalls();
+			g.setColor( Color.BLACK );
+			g.drawString( s, (this.getWidth()/2 - fm.stringWidth( s )/2), (this.getHeight()/2+fm.getHeight()/2) );
+		} catch (Exception e) {
+
+		}
+
+		//		g.drawString( s, 0, 0 );
 	}
 }
