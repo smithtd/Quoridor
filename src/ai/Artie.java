79e87d3f4160779f6030c4dc6f8bd4d67017a212
Marc <deanmarc25@yahoo.com>
2014-05-05 20:07:49 -0400

    Working on Artie. When Started he doesn't ask for an AI Identifier,
    Once connected he automatically sends it..

--- src/ai/Artie.java
+++ src/ai/Artie.java
@@ -4,6 +4,13 @@
  */
 package ai;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import network.Messages;
 import network.MoveServer;
 
 public class Artie extends MoveServer {
@@ -24,8 +31,66 @@ public class Artie extends MoveServer {
 	/** Number of walls we have */
 	int walls;
 	
+	/**
+	 * Create an instance of our AI
+	 * @param - Port to listen on 
+	 * 
+	 */
 	public Artie (int port) {
 		super(port);
+	}
+	
+	/**
+	 *  
+	 * Allows the AI to start listening on the port. Once connected it will
+	 * send the Hello message with the AI_IDENTIFIER message. It will then
+	 * wait its turn and update moves as they are seen and taken care of. 
+	 * 
+	 */
+	public void run() { 
+		
+		// Protocol says first message after connecting should be 
+		// HELLO <AI-IDENTIFIER>
+		System.out.println("Your AI Identifier is: " + AI_IDENTIFIER);
+		
+		// Remove this when AI is making its own decisions
+		this.playerInput = new Scanner(System.in);
+		System.out.println("Starting the move server");
+		this.hasWon = true;
+		try{
+			
+			// Start the listening Socket on port
+			ServerSocket server = new ServerSocket(this.port);
+			System.out.println("Waiting for a client...");
+			Socket gameClient;
+			
+			while((gameClient = server.accept()) != null) {
+				
+				server.close();
+				System.out.println("Now connected to client at: " + gameClient);
+				
+				this.clientOutput = new PrintStream(gameClient.getOutputStream());
+				
+				// Send output stream 
+				System.out.println("Sending HELLO " + AI_IDENTIFIER + " message to " + gameClient);
+				this.clientOutput.println(Messages.HELLO_MESSAGE + " " + AI_IDENTIFIER);
+				
+				// Read input that comes in from the game client
+				this.clientInput = new Scanner(gameClient.getInputStream());
+				
+				while(this.clientInput.hasNext() && this.hasWon) { 
+					String input = this.clientInput.nextLine();
+					getResponse(input);
+				}
+				
+			}
+			
+		} catch (IOException ioe) {
+			System.out.println("Connection Terminated or Error in Artie");
+		}
+		
+		this.playerInput.close();
+		
 	}
 	
 	
